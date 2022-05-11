66362981b93a7b315f90fccddf88e4b9b34cb07a
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-05-11 16:48:38 -0400

    Completed task addition functionality

--- scripts.js
+++ scripts.js
@@ -1,15 +1,47 @@
+// query DOM for form element
+const form = document.querySelector('form');
+console.log(form);
+
 // add a submit event listener on the form
+form.addEventListener('submit', function(e) {
+	
+	// stop the page from refreshing when the form is submitted
+	e.preventDefault();
+	
+	// query DOM for input element
+	const inputElement = document.getElementById('toDoItem');
+	
+	// Return error if input is empty
+	if (inputElement.value === '') {
+		alert('Please provide a task to add to your to do list')
+		return;
+	}
+	
+	// create list element to which checkbox and task will be appended
+	const liElement = document.createElement('li');
+	
+	// add checkbox icon to list element
+	liElement.innerHTML = '<i class="fa-regular fa-square"></i>';
+	
+	// create textNode containing user supplied value and append to li element
+	const capitalizedInputValue = inputElement.value.charAt(0).toUpperCase() + inputElement.value.slice(1);
+	toDoTextNode = document.createTextNode(capitalizedInputValue);
+	liElement.appendChild(toDoTextNode);
 
-    // stop the page from refreshing when the form is submitted
+	// append list item to the to-do list
+	toDoList.appendChild(liElement);
+	
+	// clear input
+	inputElement.value = '';
+	
+});
 
-    // only if the user has entered an actual task (AKA input is not empty):
-
-    // grab the user's TO DO item from the form input
-    // display TO DO on the page within an li element
-        // include a checkbox icon within the li
-    // add the li element to the ul
-
-// clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
+// add the li element to the ul
+const toDoList = document.querySelector('ul');
+toDoList.addEventListener('click', function(e) {
+	console.log(e);
+	// clicking on a task allows you to toggle between checked/unchecked (AKA done vs not done)
+});
 
 
 // BONUS LEVEL:
